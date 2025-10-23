18bcdce783fbc68bcb2a61bd84894990726ed329
Nat <nl2852@nyu.edu>
2025-10-23 01:06:14 -0400

    Update JSWAY.js

--- FEW_Projects/JSWAY/JSWAY.js
+++ FEW_Projects/JSWAY/JSWAY.js
@@ -4,6 +4,7 @@ const Rat = {
     age: 18,
     color: "spotted",
     health: 100,
+    size: 10,
 
     describe() {
         return `${this.name} is a ${this.color} rat that is ${this.age} years old with ${this.health}hp.`;
@@ -31,20 +32,39 @@ let shiftPressed = false;
 
 function setup() {
     createCanvas(400, 400);
+    textSize(18);
+    fill(0);
 
+    //Describe Rat
     console.log(Rat.describe());
+
+    
+    // Ask for a word
+    let word = prompt("Enter a word:");
+
+    //Assign types of word
+    let lengthInfo = `Length: ${word.length}`;
+    let lowerInfo = `Lowercase: ${word.toLowerCase()}`;
+    let upperInfo = `Uppercase: ${word.toUpperCase()}`;
+
+    // Display on canvas
+    text(`Word: ${word}`, 20, 100);
+    text(lengthInfo, 20, 140);
+    text(lowerInfo, 20, 180);
+    text(upperInfo, 20, 220);
+
+    //Console
+    console.log(word, word.length, word.toLowerCase(), word.toUpperCase());
+    fill("brown");
+    circle(width / 2, height / 2, Rat.size);
+    text("this is sally", width / 2, height / 2 + 20);
 }
 
 function draw() {
-    background("blue");
+    // background("blue");
+
 }
 
-// function birthday{
-
-//         if(spacePre ){
-//         console.log(Rat.age)
-//     }
-// }
 document.addEventListener('keydown',
     function (event) {
         if (event.code === "Space") {
